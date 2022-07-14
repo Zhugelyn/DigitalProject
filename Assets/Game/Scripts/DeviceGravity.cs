dc9684424234f9c9e507f1717e1b73c993bdb9cf
Zhugelyn <kibitkin_00@list.ru>
2022-07-14 15:10:38 +0300

    SortItems version up 0.0.2.1
    Добавил формулу притяжения
    объектов

---
+++ Assets/Game/Scripts/DeviceGravity.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceGravity : MonoBehaviour
+{
+    private HashSet<Rigidbody> items = new HashSet<Rigidbody>();
+    private Rigidbody rigidbody;
+    public float gravitational—onstant = 0.00f;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody != null)
+            items.Add(other.attachedRigidbody);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.attachedRigidbody != null)
+            items.Remove(other.attachedRigidbody);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var item in items)
+        {
+            Vector3 directionToDevice = (transform.position - item.position).normalized;
+
+            item.AddForce(directionToDevice * —alculateForceGravity(item));
+        }
+    }
+    public float —alculateForceGravity(Rigidbody item)
+    {
+        var distance = (transform.position - item.position).sqrMagnitude;
+        var gravityForce = (item.mass * rigidbody.mass * gravitational—onstant) / distance;
+
+        return gravityForce;
+    }
+}
+
+
